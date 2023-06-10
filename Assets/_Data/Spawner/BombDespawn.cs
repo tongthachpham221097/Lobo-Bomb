295f13444a99ff629cb8f43cb06f1103cb7076d1
Your Name <you@example.com>
2023-06-10 14:04:44 +0700

    Done FX bomb and doing fx stop if collide tilemap or obstancle

--- Assets/_Data/Spawner/BombDespawn.cs
+++ Assets/_Data/Spawner/BombDespawn.cs
@@ -6,6 +6,7 @@ using static UnityEditor.PlayerSettings;
 public class BombDespawn : LoboMonoBehaviour
 {
     [SerializeField] protected float timeDelayDespawn = 1f;
+    [SerializeField] protected int fireLength = 1;
 
     private void OnEnable()
     {
@@ -15,13 +16,32 @@ public class BombDespawn : LoboMonoBehaviour
     void BombDespawning()
     {
         BombSpawner.Instance.Despawn(transform.parent);
-        this.SpawnFX();
+        this.SpawningFX();
         
     }
-    private void SpawnFX()
+
+    void SpawningFX()
+    {
+        this.SpawnFX(transform.position);
+        for(int i = 1; i <= this.fireLength; i++)
+        {
+            Vector3 pos = transform.position + new Vector3(i, 0, 0);
+            this.SpawnFX(pos);
+
+            Vector3 pos2 = transform.position + new Vector3(0, i, 0);
+            this.SpawnFX(pos2);
+
+            Vector3 pos3 = transform.position + new Vector3(-i, 0, 0);
+            this.SpawnFX(pos3);
+
+            Vector3 pos4 = transform.position + new Vector3(0, -i, 0);
+            this.SpawnFX(pos4);
+        }
+    }    
+    private void SpawnFX(Vector3 pos)
     {
         Transform prefab = FXSpawner.Instance.RandomPrefab();
-        Transform obj = FXSpawner.Instance.Spawn(prefab, transform.position, transform.rotation);
+        Transform obj = FXSpawner.Instance.Spawn(prefab, pos, transform.rotation);
         obj.gameObject.SetActive(true);
     }
 }
