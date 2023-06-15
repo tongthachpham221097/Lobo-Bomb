87943c72045e3c5043b73790642794892ca8a65d
Your Name <you@example.com>
2023-06-15 18:48:07 +0700

    Done FX collide with obstacle can't despawn

--- Assets/_Data/Spawner/FXSpawner/FXSpawner.cs
+++ Assets/_Data/Spawner/FXSpawner/FXSpawner.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class FXSpawner : Spawner
@@ -8,29 +9,33 @@ public class FXSpawner : Spawner
     public static string fx1 = "FX_1";
 
     [SerializeField] private int fireLength = 2;
-    private Vector3 bombPosition;
-    private Vector3Int bombPosTilemapGround;
+    private Vector3 _bombPosition;
+    private Vector3Int _bombPosTilemapGround;
     private Vector3 offsetCenter = new Vector3(0.5f, 0, 0);
+
+    private Dictionary<Vector3Int, bool> _spawnDirections = new Dictionary<Vector3Int, bool>();
 
     public void GetBombPosition(Vector3 bombPos)
     {
-        this.bombPosition = bombPos;
+        this._bombPosition = bombPos;
         this.GetBombTilePosition();
     }
 
     void GetBombTilePosition()
     {
-        this.bombPosTilemapGround = this.spawnerCtrl.GameCtrl.GridSystemCtrl.TilemapBgOverWalls.WorldToCell(this.bombPosition);
+        this._bombPosTilemapGround = this.spawnerCtrl.GameCtrl.GridSystemCtrl.TilemapBgOverWalls.WorldToCell(this._bombPosition);
     }
 
     public virtual void Spawning()
     {
-        this.SpawnFX(this.bombPosTilemapGround + this.offsetCenter);
+        this.SpawnFX(this._bombPosTilemapGround + this.offsetCenter);
         this.SpawnFXInAllDirections();
     }
 
     void SpawnFXInAllDirections()
     {
+        this._spawnDirections.Clear();
+
         SpawnFXInDirection(Vector3Int.up);
         SpawnFXInDirection(Vector3Int.down);
         SpawnFXInDirection(Vector3Int.left);
@@ -41,8 +46,9 @@ public class FXSpawner : Spawner
     {
         for (int i = 1; i <= this.fireLength; i++)
         {
-            Vector3Int spawnPosition = this.bombPosTilemapGround + (direction * i);
-            if (!this.spawnerCtrl.SpawnPointsInWalls.CheckSpawnPoints(spawnPosition)) continue;
+            Vector3Int spawnPosition = this._bombPosTilemapGround + (direction * i);
+            if (this.CheckTilemapWalls(spawnPosition)) continue;
+            if (this.CheckTilemapObstacle(spawnPosition, direction)) continue;
             SpawnFX(spawnPosition + this.offsetCenter);
         }
     }
@@ -52,5 +58,23 @@ public class FXSpawner : Spawner
         Transform prefab = this.RandomPrefab();
         Transform obj = this.Spawn(prefab, pos, transform.rotation);
         obj.gameObject.SetActive(true);
+    }
+
+    bool CheckTilemapWalls(Vector3Int spawnPosition)
+    {
+        if (!this.spawnerCtrl.SpawnPointsInWalls.CheckSpawnPoints(spawnPosition)) return true;
+        return false;
+    }
+    
+    bool CheckTilemapObstacle(Vector3Int spawnPosition, Vector3Int direction)
+    {
+        if(this._spawnDirections.ContainsKey(direction) && this._spawnDirections[direction] == true) return true;
+        
+        TileBase tile = this.spawnerCtrl.GameCtrl.GridSystemCtrl.Destructibles.GetTile(spawnPosition);
+        if (tile == null) return false;
+
+        this._spawnDirections[direction] = true;
+        
+        return true;
     }
 }
--- Assets/_Data/Spawner/SpawnerCtrl.cs
+++ Assets/_Data/Spawner/SpawnerCtrl.cs
@@ -11,15 +11,12 @@ public class SpawnerCtrl : BaseCtrl
     [SerializeField] private FXSpawner _fxSpawner;
     public FXSpawner FXSpawner => _fxSpawner;
 
-    [SerializeField] private ObstancleSpawner _obstancleSpawner;
-    public ObstancleSpawner ObstancleSpawner => _obstancleSpawner;
     protected override void LoadComponents()
     {
         base.LoadComponents();
         this.LoadSpawnPointsInWalls();
         this.LoadBombSpawner();
         this.LoadFXSpawner();
-        this.LoadObstancleSpawner();
     }
 
     void LoadSpawnPointsInWalls()
@@ -41,12 +38,5 @@ public class SpawnerCtrl : BaseCtrl
         if (this._fxSpawner != null) return;
         this._fxSpawner = GetComponentInChildren<FXSpawner>();
         Debug.LogWarning(transform.name + ": LoadFXSpawner", gameObject);
-    }
-
-    void LoadObstancleSpawner()
-    {
-        if (this._obstancleSpawner != null) return;
-        this._obstancleSpawner = GetComponentInChildren<ObstancleSpawner>();
-        Debug.LogWarning(transform.name + ": LoadObstancleSpawner", gameObject);
     }
 }
